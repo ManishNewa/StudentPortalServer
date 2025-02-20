b7d04f92d4ab0a6582c2a7dd43c378952225cd8b
Manish Maharjan <manishnewar26@gmail.com>
2025-02-20 21:45:56 +0000

    Initial setup for backend server

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express'
+import path from 'path'
+import apiRoutes from '../routes/api'
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '../public')))
+
+// API routes
+app.use('/api', apiRoutes)
+
+// Serve the Vue app for all other routes
+// app.get('*', (req: Request, res: Response) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'))
+// })
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Node.js!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})

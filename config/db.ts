e9d88188d9e63462c0e9d45b05313b7a32fda201
Manish Maharjan <manishnewar26@gmail.com>
2025-02-21 21:39:31 +0000

    database configuration for local

---
+++ config/db.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
+
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '3306'),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: process.env.NODE_ENV === 'development',
+    logging: true,
+    entities: ['models/**/*.ts'],
+    migrations: ['migrations/**/*.ts'],
+});

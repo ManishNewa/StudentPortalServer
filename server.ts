05b8675d8848becb5f7a14fa87bae94a30d58d25
Manish Maharjan <manishnewar26@gmail.com>
2025-02-21 21:48:29 +0000

    Database initiated in server file

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+import { AppDataSource } from './config/db';
 import path from 'path'
 import apiRoutes from './routes/api'
 
@@ -16,10 +17,17 @@ app.use('/api', apiRoutes)
 //   res.sendFile(path.join(__dirname, '../public/index.html'))
 // })
 
+// Initialize DB
+AppDataSource.initialize()
+  .then(() => {
+    console.log('DB connected');
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`)
+    })
+  })
+  .catch((error) => console.log('Database connection failed:', error));
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Node.js!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})

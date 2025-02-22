f6ea599c179504bb727e2617bd1acb111a8eb1d3
Manish Maharjan <manishnewar26@gmail.com>
2025-02-22 21:41:58 +0000

    enums added for UserRoles and AuthProviders

---
+++ constants/enums.ts
@@ -0,0 +1,14 @@
+export const enum UserRole {
+    STUDENT = 'student',
+    LANDLORD = 'landlord',
+    EMPLOYER = 'employer',
+    ADMIN = 'admin',
+    SUPERADMIN = 'superadmin'
+}
+
+export const enum AuthProvider {
+    LOCAL = 'local',
+    GOOGLE = 'google',
+    FACEBOOK = 'facebook',
+    WHATSAPP = 'whatsapp'
+}

54bf0b18a1476b946757acee526f085d4f224b13
Manish Maharjan <manishnewar26@gmail.com>
2025-02-23 19:15:24 +0000

    app configured and content moved to app, server  configured to run the application, basic auth route configured

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+
+import authRoutes from './routes/auth-routes';
+import sequelize from './config/db';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', authRoutes);
+
+sequelize.sync().then(() => {
+    console.log('Database synced');
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,10 @@
-import 'reflect-metadata';
-import express, { Request, Response } from 'express';
-import { AppDataSource } from './config/db';
-import path from 'path';
-import apiRoutes from './routes/api';
+import app from './app';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, '../public')));
-
-// API routes
-app.use('/api', apiRoutes);
-
-// Initialize DB
-AppDataSource.initialize()
-    .then(() => {
-        console.log('DB connected');
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        });
-    })
-    .catch((error) => console.log('Database connection failed:', error));
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Node.js!');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });

1aca4a230ceaeec79dd438732abbfa7482bf760f
Manish Maharjan <manishnewar26@gmail.com>
2025-02-23 19:30:11 +0000

    migration added to create user table

---
+++ src/migrations/create-user-table.ts
@@ -0,0 +1,58 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+import { UserRole, AuthProvider } from '../constants/enums';
+
+module.exports = {
+    up: async (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
+        await queryInterface.createTable('Users', {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            phone: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            role: {
+                type: Sequelize.ENUM(...Object.values(UserRole)),
+                defaultValue: UserRole.GUEST,
+            },
+            authProvider: {
+                type: Sequelize.ENUM(...Object.values(AuthProvider)),
+                allowNull: true,
+            },
+            providerId: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            verified: {
+                type: Sequelize.BOOLEAN,
+                defaultValue: false,
+            },
+            verificationToken: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+    down: async (queryInterface: QueryInterface) => {
+        await queryInterface.dropTable('Users');
+    },
+};

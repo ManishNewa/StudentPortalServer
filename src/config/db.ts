f077bb4af33b4879f9985deec85a2fb1134d8442
Manish Maharjan <manishnewar26@gmail.com>
2025-02-23 19:48:31 +0000

    Indentation fixes

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  }
+    process.env.DB_NAME!,
+    process.env.DB_USER!,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+    },
 );
 
 export default sequelize;

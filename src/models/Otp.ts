0f37ce65beba36d8acb523c588783a5265663641
Manish Maharjan <manishnewar26@gmail.com>
2025-02-25 22:34:41 +0000

    Otp model reconfigured and renamed

--- src/models/Otp.ts
+++ src/models/Otp.ts
@@ -1,39 +1,58 @@
-import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-    JoinColumn,
-} from 'typeorm';
-import { User } from './User';
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/db';
 
-@Entity()
-export class OTP {
-    @PrimaryGeneratedColumn()
-    id!: number;
+import User from './User';
 
-    @Column({
-        type: 'varchar',
-        length: 6,
-    })
-    code!: string;
+class Otp extends Model {
+    public id!: number;
+    public userId!: number;
+    public code!: string;
+    public expiresAt!: Date;
+    public isUsed!: boolean;
 
-    @Column({
-        type: 'timestamp',
-    })
-    expiry!: Date;
-
-    @Column({
-        type: 'boolean',
-        default: false,
-    })
-    isUsed!: boolean;
-
-    // Many-to-One relationship with User
-    @ManyToOne(() => User, (user) => user.otps)
-    @JoinColumn({ name: 'userId' })
-    user!: User;
-
-    @Column()
-    userId!: number; // Foreign key to User
+    // Timestamps
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
 }
+
+Otp.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'id',
+            },
+        },
+        code: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        expiresAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        isUsed: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Otp',
+        timestamps: true,
+    },
+);
+
+// Relationships
+User.hasMany(Otp, { foreignKey: 'userId' });
+Otp.belongsTo(User, { foreignKey: 'userId' });
+
+export default Otp;

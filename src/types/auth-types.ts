ea23a89e4c84979ff92144ecb14f66eb200060a5
Manish Maharjan <manishnewar26@gmail.com>
2025-02-26 22:54:24 +0000

    interfaces created for auth types

---
+++ src/types/auth-types.ts
@@ -0,0 +1,23 @@
+export interface RegisterRequest {
+    email: string;
+    password: string;
+    phone?: string;
+    role?: string;
+    authProvider?: string;
+    providerId?: string;
+}
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface SocialLoginRequest {
+    idToken?: string;
+    accessToken?: string;
+}
+
+export interface OtpRequest {
+    email: string;
+    otp?: string;
+}

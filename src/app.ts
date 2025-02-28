ae27e558477775e98bb6de45e0e9c55463f2bde7
Manish Maharjan <manishnewar26@gmail.com>
2025-02-28 23:21:02 +0000

    session configured in app file

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,19 @@ import cors from 'cors';
 
 import authRoutes from './routes/auth-routes';
 import sequelize from './config/db';
+import { sessionConfig } from './config/session';
 
 const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(sessionConfig);
 
+// Routes
 app.use('/api/auth', authRoutes);
 
+// DB
 sequelize.sync().then(() => {
     console.log('Database synced');
 });

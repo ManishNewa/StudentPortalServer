49251611ff93a4a9c42bf206f9364873180211c7
Manish Maharjan <manishnewar26@gmail.com>
2025-02-28 23:19:37 +0000

    redis session configuration setup

---
+++ src/config/session.ts
@@ -0,0 +1,19 @@
+import session from 'express-session';
+import Redis from 'ioredis';
+import { RedisStore } from 'connect-redis';
+
+const redisClient = new Redis();
+
+export const sessionConfig = session({
+    store: new RedisStore({ client: redisClient }),
+    secret:
+        process.env.REDIS_SECRET_KEY ||
+        'hV3Gd5vF8cXU4n8GyLVP5Qw1dUz+RzX6ST9cC2ZtGFE=',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+});

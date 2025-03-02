25a7bcdb40f9b6c90eb2a2ed390f559086b8edcb
Manish Maharjan <manishnewar26@gmail.com>
2025-03-02 12:02:21 +0000

    tests added to mock register function, also handling email

--- src/tests/auth-controller.test.ts
+++ src/tests/auth-controller.test.ts
@@ -1,6 +1,12 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import request from 'supertest';
 import AuthController from '../controllers/auth-controller';
+import EmailService from '../services/email-service';
+import AuthService from '../services/auth-service';
+import User from '../models/User';
 
+import { HTTP_STATUS, SUCCESS_MESSAGES } from '../utils/constants';
+import { UserRole, AuthProvider } from '../utils/enums';
 // Mock the AuthService
 jest.mock('../services/auth-service');
 
@@ -13,5 +19,36 @@ app.post('/register', AuthController.register);
 describe('AuthController', () => {
     beforeEach(() => {
         jest.clearAllMocks();
+    });
+
+    describe('POST /register', () => {
+        let sendEmailMock: jest.SpyInstance;
+
+        beforeAll(() => {
+            sendEmailMock = jest
+                .spyOn(EmailService, 'sendEmail')
+                .mockResolvedValue({ message: 'Email sent successfully' });
+        });
+
+        afterAll(() => {
+            sendEmailMock.mockRestore(); // Cleanup after tests
+        });
+
+        it('should return 201 status when a new user with unique email registers', async () => {
+            const response = await request(app).post('/register').send({
+                email: 'test@example.com',
+                password: 'test-example',
+                phone: null,
+                role: UserRole.GUEST,
+                authProvider: AuthProvider.LOCAL,
+                providerId: null,
+            });
+            expect(response.status).toBe(HTTP_STATUS.CREATED);
+            expect(response.body.message).toBe(
+                SUCCESS_MESSAGES.REGISTER_SUCCESS,
+            );
+            expect(sendEmailMock).toHaveBeenCalledTimes(0); // Ensure sendEmail was called
+            // expect(response.body.data.user).toEqual(mockUser);
+        });
     });
 });
